164e966da1314317018d47bd2d31c7ce2b12ae3b
Adam Fahie <adam.fahie3@nordstrom.com>
2018-01-24 11:11:09 -0800

    Adam - Terraform wrapper script

---
+++ terraform.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$#" -ne 2 ]
+then
+printf "Two parameters required. \n1. Terraform action (e.g. plan) \n2. Environment to run against (e.g. sandbox)"
+exit 1
+fi
+
+TERRAFORM_ACTION=$1
+ENVIRONMENT=$2
+
+export GOOGLE_APPLICATION_CREDENTIALS=~/.google_cloud/account.json
+cd environment-infrastructure
+terraform init
+terraform ${TERRAFORM_ACTION} -var-file=../variables/audit/environment.tfvars
