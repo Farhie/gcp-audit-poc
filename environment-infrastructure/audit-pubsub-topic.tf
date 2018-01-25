f73e5d5c95a82d5e04381e6bd54cee943cc3f54c
Adam Fahie <adam.fahie3@nordstrom.com>
2018-01-24 20:28:14 -0800

    Adam - attemptign to paramterise JS files

--- environment-infrastructure/audit-pubsub-topic.tf
+++ environment-infrastructure/audit-pubsub-topic.tf
@@ -3,10 +3,51 @@ resource "google_pubsub_topic" "audit_poc_topic" {
   project = "${var.project}"
 }
 
+data "template_file" "set_project_in_cloud_function" {
+  template = "${file("../cloud-functions/triggering-off-pubsub-insert/template/index.template")}"
+
+  vars {
+    gcp_project = "${var.project}"
+  }
+}
+
+//resource "local_file" "js" {
+//  content      = "${data.template_file.set_project_in_cloud_function.rendered}"
+//  filename = "../cloud-functions/triggering-off-pubsub-insert/function/index.js"
+//}
+//data "archive_file" "cloud_function_archive" {
+//  type        = "zip"
+//  source_dir  = "../cloud-functions/triggering-off-pubsub-insert/function/"
+//  output_path = "../cloud-functions/triggering-off-pubsub-insert/message-ingress-pubsub.zip"
+//  depends_on  = ["local_file.js"]
+//}
+
+
+data "archive_file" "cloud_function_archive" {
+  type        = "zip"
+  output_path = "../cloud-functions/triggering-off-pubsub-insert/message-ingress-pubsub.zip"
+
+  source {
+    content  = "${data.template_file.set_project_in_cloud_function.rendered}"
+    filename = "index.js"
+  }
+
+  source {
+    content  = "${file("../cloud-functions/triggering-off-pubsub-insert/function/package.json")}"
+    filename = "package.json"
+  }
+
+  source {
+    content  = "${file("../cloud-functions/triggering-off-pubsub-insert/function/package-lock.json")}"
+    filename = "package.json"
+  }
+}
+
 resource "google_storage_bucket_object" "message_ingress_to_pubsub_archive" {
   name   = "message-ingress-pubsub.zip"
   bucket = "${google_storage_bucket.cloud_functions_bucket.name}"
-  source = "../cloud-functions/triggering-off-pubsub-insert/message-ingress-pubsub.zip"
+  source = "${data.archive_file.cloud_function_archive.output_path}"
+  depends_on = ["data.archive_file.cloud_function_archive"]
 }
 
 resource "google_cloudfunctions_function" "update_on_insert_to_pubsub_function" {
--- environment-infrastructure/spanner-database.tf
+++ environment-infrastructure/spanner-database.tf
@@ -1,13 +1,13 @@
-resource "google_spanner_instance" "audit_poc_spanner_instance" {
-  config       = "regional-us-central1"
-  display_name = "audit-poc-instance"
-  name         = "audit-poc-instance"
-  num_nodes    = 1
-  project      = "${var.project}"
-}
-
-resource "google_spanner_database" "audit_poc_spanner_database" {
-  instance = "${google_spanner_instance.audit_poc_spanner_instance.name}"
-  name     = "audit-poc-spanner-table"
-  project  = "${var.project}"
-}
+//resource "google_spanner_instance" "audit_poc_spanner_instance" {
+//  config       = "regional-us-central1"
+//  display_name = "audit-poc-instance"
+//  name         = "audit-poc-instance"
+//  num_nodes    = 1
+//  project      = "${var.project}"
+//}
+//
+//resource "google_spanner_database" "audit_poc_spanner_database" {
+//  instance = "${google_spanner_instance.audit_poc_spanner_instance.name}"
+//  name     = "audit-poc-spanner-table"
+//  project  = "${var.project}"
+//}
